b4e4470cfb29df54d2884fc41908e208a816f5bd
soyab-mostofa <soyabmostofa@gmail.com>
2024-08-07 14:35:03 +0600

    bug fixed and added email

--- src/lib/mailer.ts
+++ src/lib/mailer.ts
@@ -7,7 +7,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export const sendMail = async () => {
   try {
     const { data, error } = await resend.emails.send({
-      from: "Acme <onboarding@resend.dev>",
+      from: "Inkam <hello@inkam.app>",
       to: ["inkamlimited@gmail.com"],
       subject: "Test mail",
       react: Email({ url: "John@inkam.app" }) as React.ReactElement,

16d519e1b41c0ce2e337c7cca7459cd29dce4db2
soyab-mostofa <soyabmostofa@gmail.com>
2024-10-15 17:19:59 +0600

    fix layout

--- src/app/dashboard/payments/page.tsx
+++ src/app/dashboard/payments/page.tsx
@@ -20,14 +20,13 @@ const PaymentsPage = async ({
 }: {
   searchParams: { [key: string]: string | string[] | undefined };
 }) => {
-  const page = Number(searchParams["page"] ?? "1");
-  const per_page = Number(searchParams["per_page"] ?? "10");
-
-  const skip = (page - 1) * per_page;
-
   // Fetch total count of agents to determine the last page
-  const totalAgents = await prisma.agent.count();
-  const totalPages = Math.ceil(totalAgents / per_page);
+  const totalPayment = await prisma.transaction.count({
+    where: {
+      status: "PROVIDED",
+      isPaid: false,
+    },
+  });
 
   const transactions = await prisma.transaction.groupBy({
     by: ["agentId", "agentName"],
@@ -49,40 +48,6 @@ const PaymentsPage = async ({
         <SuspenseLoader>
           <AllPayments transactions={transactions} />
         </SuspenseLoader>
-        <Pagination>
-          <PaginationContent>
-            <PaginationItem>
-              {page > 1 && (
-                <PaginationPrevious
-                  href={{
-                    pathname: "/dashboard/agents",
-                    query: {
-                      page: page - 1,
-                      per_page: per_page,
-                    },
-                  }}
-                />
-              )}
-            </PaginationItem>
-            <PaginationItem></PaginationItem>
-            <PaginationItem>
-              <PaginationEllipsis />
-            </PaginationItem>
-            <PaginationItem>
-              {page < totalPages && (
-                <PaginationNext
-                  href={{
-                    pathname: "/dashboard/agents",
-                    query: {
-                      page: page + 1,
-                      per_page: per_page,
-                    },
-                  }}
-                />
-              )}
-            </PaginationItem>
-          </PaginationContent>
-        </Pagination>
       </div>
 
       {transactions.length > 0 ? (
